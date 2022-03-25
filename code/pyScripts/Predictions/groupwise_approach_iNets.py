0ea0e81690562025f70afc4ce6f0205a4e71fb59
Alexis Porter <alexis.porter1313@gmail.com>
2022-03-25 17:10:36 -0500

    inetworks

---
+++ code/pyScripts/Predictions/groupwise_approach_iNets.py
@@ -0,0 +1,312 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import LeaveOneOut
+from sklearn.linear_model import RidgeClassifier
+import numpy as np
+import pandas as pd
+import glob
+from sklearn.metrics import confusion_matrix
+from statistics import mean
+import scipy.io
+import os
+import sys
+# Initialization of directory information:
+thisDir = '/projects/b1081/member_directories/aporter/TaskRegScripts/SurfaceResiduals/'
+dataDir = thisDir
+outDir = thisDir + 'output/'
+# Subjects and tasks
+#thisDir = os.path.expanduser('~/Desktop/Porteretal_taskprediction/')
+#dataDir = thisDir + 'data/corrmats/iNetworks/'
+#outDir = thisDir + 'output/results/Ridge/'
+taskList=['slowreveal','mixed']
+sesList = ['ses-1','ses-2','ses-3','ses-4']
+
+
+
+def model(train_task):
+    """
+    Preparing machine learning model with appropriate data
+
+    Parameters
+    -------------
+    train_task : str
+            Task name for training
+    test_task : str
+            Task name for testing
+
+    Returns
+    -------------
+    within_score : float
+            Average accuracy of all folds leave one sub out of a given task
+    within_sens : float
+            Sensitivity score for model within task
+    within spec : float
+            Specificity score for model within task
+    btn_score : float
+            Average accuracy of all folds leave one sub out of a given task
+    btn_sens : float
+            Sensitivity score for model within task
+    btn spec : float
+            Specificity score for model within task
+
+    """
+
+    clf=RidgeClassifier()
+    loo = LeaveOneOut()
+    SSacc_folds=[]
+    dfDS=pd.DataFrame()
+    #get all subs for a given task
+    #make list based on available subjects in task folder
+    subList=[]
+    pattern = dataDir+train_task+"/*"
+    files = [os.path.basename(x) for x in glob.glob(pattern)]
+    for i in files:
+        subList.append(i.split('_', 1)[0])
+
+    data=np.array(subList,dtype='<U61')
+    sessions=np.array(sesList,dtype='<U61')
+    for  train, test in loo.split(data): #test on all other subj
+        train_sub = data[test] #testing subj
+        test_subs = data[train] #training subjs
+        #randomly collects one run one session per subj
+        testing_set_task = iNets_OS_onerun(train_task, test_subs)
+        testing_set_rest = iNets_OS_onerun('rest', test_subs)
+        #make sure rest and task sets are the same amount
+        y_test_task=np.ones(testing_set_task.shape[0])
+        y_test_rest=np.zeros(testing_set_rest.shape[0])
+        ytest=np.concatenate((y_test_task,y_test_rest))
+        Xtest=np.concatenate((testing_set_task,testing_set_rest))
+        #left out sub
+        LOS_task=iNetOpenRun(train_task, train_sub[0])
+        LOS_rest=iNetOpenRun('rest', train_sub[0])
+        task=np.ones(LOS_task.shape[0])
+        rest=np.zeros(LOS_rest.shape[0])
+        y=np.concatenate((task,rest))
+        X=np.concatenate((LOS_task,LOS_rest))
+        clf.fit(Xtest,ytest)
+        #Same subject
+        SSscores=clf.score(X,y)
+        SSacc_folds.append(SSscores)
+
+    #SStotal_acc=(mean(SSacc_folds)
+    dfDS['acc'] = SSacc_folds
+    dfDS.to_csv(outDir+train_task+'_iNets_acc_groupwise.csv',index=False)
+
+
+def iNets_OS_onerun(train_task, test_subs):
+    """
+    Calculate FC matrices for all subs in a given task keeping one run randomly
+    Parameters
+    -----------
+    df : str
+        Path to file
+    Returns
+    -----------
+    ds : 2D upper triangle FC measures in (roi, days) format
+
+    """
+    result_arr = []
+    #loop through and append all test sets do each task/rest separate
+    for sub in test_subs:
+        all_sub_mats = iNetOpenRun(train_task, sub)
+        result_arr.append(all_sub_mats)
+    result_arr = np.concatenate(result_arr)
+    return result_arr
+
+def iNetOpenRun(train_task, sub):
+    """
+    Convert matlab files into upper triangle np.arrays for a given sub (one run)
+    Parameters
+    -----------
+    df : str
+        Path to file
+    Returns
+    -----------
+    ds : 2D upper triangle FC measures in (roi, days) format
+
+    """
+    #find all files
+    files = glob.glob(dataDir+train_task+'/'+sub+'_ses-*')
+    nsess = len(files) #FC matrices
+    nrois=333
+    ds=np.empty((nsess, int(nrois*(nrois-1)/2)))
+    count=0
+    for f in files:
+    #Consistent parameters to use for editing datasets
+
+        #Load FC file
+        fileFC=scipy.io.loadmat(f)
+
+        #Convert to numpy array
+        fileFC=np.array(fileFC['parcel_corrmat'])
+        #Replace nans and infs with zero
+        fileFC=np.nan_to_num(fileFC)
+        #Index upper triangle of matrix
+        mask=np.triu_indices(nrois,1)
+
+    #Loop through all 10 days to reshape correlations into linear form
+
+        tmp=fileFC[:,:]
+        ds[count]=tmp[mask]
+        count=count+1
+    mask = (ds == 0).all(1)
+    column_indices = np.where(mask)[0]
+    df = ds[~mask,:]
+    #randomly choose 1 run from 1 session
+    df=df[np.random.choice(df.shape[0], 1, replace=False), :]
+    return df
+
+#for task in taskList:
+#    model(task)
+
+def MCmodel():
+    """
+    Preparing machine learning model with appropriate data
+
+    Parameters
+    -------------
+    train_task : str
+            Task name for training
+    test_task : str
+            Task name for testing
+
+    Returns
+    -------------
+    within_score : float
+            Average accuracy of all folds leave one sub out of a given task
+    within_sens : float
+            Sensitivity score for model within task
+    within spec : float
+            Specificity score for model within task
+    btn_score : float
+            Average accuracy of all folds leave one sub out of a given task
+    btn_sens : float
+            Sensitivity score for model within task
+    btn spec : float
+            Specificity score for model within task
+
+    """
+
+    clf=RidgeClassifier()
+    loo = LeaveOneOut()
+    SSacc_folds=[]
+    dfDS=pd.DataFrame()
+    #get all subs for a given task
+    #make list based on available subjects in task folder
+    subList=[]
+    pattern = dataDir+"mixed/*"
+    files = [os.path.basename(x) for x in glob.glob(pattern)]
+    for i in files:
+        subList.append(i.split('_', 1)[0])
+
+    data=np.array(subList,dtype='<U61')
+    sessions=np.array(sesList,dtype='<U61')
+    for  train, test in loo.split(data): #test on all other subj
+        train_sub = data[test] #testing subj
+        test_subs = data[train] #training subjs
+        #randomly collects one run one session per subj
+        testing_set_mix = iNets_OS_onerun('mixed', test_subs)
+        testing_set_slow = iNets_OS_onerun('slowreveal', test_subs)
+        testing_set_rest = iNets_OS_onerun('rest', test_subs)
+        #make sure rest and task sets are the same amount
+        y_test_slow=np.full(testing_set_slow.shape[0],2)
+        y_test_mix=np.ones(testing_set_mix.shape[0])
+        y_test_rest=np.zeros(testing_set_rest.shape[0])
+        ytest=np.concatenate((y_test_slow,y_test_mix,y_test_rest))
+        Xtest=np.concatenate((testing_set_slow,testing_set_mix,testing_set_rest))
+        #left out sub
+        LOS_slow=iNetOpenRun('slowreveal', train_sub[0])
+        LOS_mix=iNetOpenRun('mixed', train_sub[0])
+        LOS_rest=iNetOpenRun('rest', train_sub[0])
+        slow=np.full(LOS_slow.shape[0],2)
+        mix=np.ones(LOS_mix.shape[0])
+        rest=np.zeros(LOS_rest.shape[0])
+        y=np.concatenate((slow, mix,rest))
+        X=np.concatenate((LOS_slow,LOS_mix,LOS_rest))
+        clf.fit(Xtest,ytest)
+        #Same subject
+        SSscores=clf.score(X,y)
+        SSacc_folds.append(SSscores)
+
+    #SStotal_acc=(mean(SSacc_folds)
+    dfDS['acc'] = SSacc_folds
+    dfDS.to_csv(outDir+'MC_iNets_acc_groupwise.csv',index=False)
+
+
+def Binarymodel():
+    """
+    Preparing machine learning model with appropriate data
+
+    Parameters
+    -------------
+    train_task : str
+            Task name for training
+    test_task : str
+            Task name for testing
+
+    Returns
+    -------------
+    within_score : float
+            Average accuracy of all folds leave one sub out of a given task
+    within_sens : float
+            Sensitivity score for model within task
+    within spec : float
+            Specificity score for model within task
+    btn_score : float
+            Average accuracy of all folds leave one sub out of a given task
+    btn_sens : float
+            Sensitivity score for model within task
+    btn spec : float
+            Specificity score for model within task
+
+    """
+
+    clf=RidgeClassifier()
+    loo = LeaveOneOut()
+    SSacc_folds=[]
+    dfDS=pd.DataFrame()
+    #get all subs for a given task
+    #make list based on available subjects in task folder
+    subList=[]
+    pattern = dataDir+"mixed/*"
+    files = [os.path.basename(x) for x in glob.glob(pattern)]
+    for i in files:
+        subList.append(i.split('_', 1)[0])
+
+    data=np.array(subList,dtype='<U61')
+    sessions=np.array(sesList,dtype='<U61')
+    for  train, test in loo.split(data): #test on all other subj
+        train_sub = data[test] #testing subj
+        test_subs = data[train] #training subjs
+        #randomly collects one run one session per subj
+        testing_set_mix = iNets_OS_onerun('mixed', test_subs)
+        testing_set_slow = iNets_OS_onerun('slowreveal', test_subs)
+        testing_set_rest = iNets_OS_onerun('rest', test_subs)
+        #make sure rest and task sets are the same amount
+        y_test_slow=np.ones(testing_set_slow.shape[0])
+        y_test_mix=np.ones(testing_set_mix.shape[0])
+        y_test_rest=np.zeros(testing_set_rest.shape[0])
+        ytest=np.concatenate((y_test_slow,y_test_mix,y_test_rest))
+        Xtest=np.concatenate((testing_set_slow,testing_set_mix,testing_set_rest))
+        #left out sub
+        LOS_slow=iNetOpenRun('slowreveal', train_sub[0])
+        LOS_mix=iNetOpenRun('mixed', train_sub[0])
+        LOS_rest=iNetOpenRun('rest', train_sub[0])
+        slow=np.ones(LOS_slow.shape[0])
+        mix=np.ones(LOS_mix.shape[0])
+        rest=np.zeros(LOS_rest.shape[0])
+        y=np.concatenate((slow, mix,rest))
+        X=np.concatenate((LOS_slow,LOS_mix,LOS_rest))
+        clf.fit(Xtest,ytest)
+        #Same subject
+        SSscores=clf.score(X,y)
+        SSacc_folds.append(SSscores)
+
+    #SStotal_acc=(mean(SSacc_folds)
+    dfDS['acc'] = SSacc_folds
+    dfDS.to_csv(outDir+'Binary_iNets_acc_groupwise.csv',index=False)
+
+#MCmodel()
--- code/pyScripts/Predictions/iNets.py
+++ code/pyScripts/Predictions/iNets.py
@@ -2,29 +2,21 @@ from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import LeaveOneOut
 from sklearn.linear_model import RidgeClassifier
 import numpy as np
-import os
-import sys
 import pandas as pd
-import itertools
 import glob
-import reshape
 from sklearn.metrics import confusion_matrix
 from statistics import mean
 import scipy.io
-#import results
-import warnings
-warnings.filterwarnings("ignore")
+import os
+import sys
 # Initialization of directory information:
-thisDir = os.path.expanduser('~/Desktop/Porteretal_taskprediction/')
-dataDir = thisDir + 'data/corrmats/iNetworks/'
-outDir = thisDir + 'output/results/'
+thisDir = '/projects/b1081/member_directories/aporter/TaskRegScripts/SurfaceResiduals/'
+dataDir = thisDir
+outDir = thisDir + 'output/'
 # Subjects and tasks
 taskList=['slowreveal','mixed']
 sesList = ['ses-1','ses-2','ses-3','ses-4']
 
-def runAll():
-    for task in taskList:
-        classifyDS_INET(task)
 
 def classifyDS_INET(train_task):
     """
@@ -61,8 +53,10 @@ def classifyDS_INET(train_task):
         OSacc_folds =[]
         train_sub = data[test] #training subj
         test_subs = data[train] #testing subjs
-        testing_set_task = reshape.iNets_OS(train_task, test_subs)
-        testing_set_rest = reshape.iNets_OS('rest', test_subs)
+        testing_set_task = iNets_OS(train_task, test_subs)
+        testing_set_rest = iNets_OS('rest', test_subs)
+        #make sure rest and task sets are the same amount
+        testing_set_rest=testing_set_rest[np.random.choice(testing_set_rest.shape[0], testing_set_task.shape[0], replace=False), :]
         y_test_task=np.ones(testing_set_task.shape[0])
         y_test_rest=np.zeros(testing_set_rest.shape[0])
         ytest=np.concatenate((y_test_task,y_test_rest))
@@ -70,10 +64,10 @@ def classifyDS_INET(train_task):
         for train_ses, test_ses in loo.split(sessions): #leave one session out
             train_sub_ses = sessions[train_ses]
             val_sub_ses = sessions[test_ses]
-            taskFC = reshape.iNets_SS(train_task, train_sub[0], train_sub_ses)
-            val_task = reshape.iNetOpenSes(train_task, train_sub[0], val_sub_ses[0])
-            restFC = reshape.iNets_SS('rest', train_sub[0], train_sub_ses)
-            val_rest = reshape.iNetOpenSes('rest', train_sub[0], val_sub_ses[0])
+            taskFC = iNets_SS(train_task, train_sub[0], train_sub_ses)
+            val_task = iNetOpenSes(train_task, train_sub[0], val_sub_ses[0])
+            restFC = iNets_SS('rest', train_sub[0], train_sub_ses)
+            val_rest = iNetOpenSes('rest', train_sub[0], val_sub_ses[0])
 
             #make sure rest and task training sets are the same amount
             restFC_sample=restFC[np.random.choice(restFC.shape[0], taskFC.shape[0], replace=False), :]
@@ -82,11 +76,11 @@ def classifyDS_INET(train_task):
             r=np.zeros(restFC_sample.shape[0], dtype=int)
             y = np.concatenate((t,r))
             x = np.concatenate((taskFC, restFC_sample))
-            #print(train_sub[0], val_task.shape[0],val_rest.shape[0])
+            val_rest_sample=val_rest[np.random.choice(val_rest.shape[0],val_task.shape[0], replace = False, :] #match task amount
             val_t = np.ones(val_task.shape[0], dtype = int)
-            val_r=np.zeros(val_rest.shape[0], dtype=int)
+            val_r=np.zeros(val_rest_sample.shape[0], dtype=int)
             y_val = np.concatenate((val_t,val_r))
-            X_val= np.concatenate((val_task,val_rest))
+            X_val= np.concatenate((val_task,val_rest_sample))
             clf.fit(x,y)
             #Same subject
             SSscores=clf.score(X_val,y_val)
@@ -101,7 +95,7 @@ def classifyDS_INET(train_task):
     dfDS['SS'] = SStotal_acc
     dfDS['OS'] = OStotal_acc
     df=pd.melt(dfDS,id_vars=['subs'],value_vars=['SS','OS'],var_name='Analysis',value_name='acc')
-    df.to_csv(outDir+'Ridge/single_task/'+train_task+'_iNets_acc.csv',index=False)
+    df.to_csv(outDir+train_task+'_iNets_acc_matched_sample.csv',index=False)
 
 
 
@@ -141,9 +135,9 @@ def classifyALL_Binary_INET():
         OSacc_folds =[]
         train_sub = data[test] #training subj
         test_subs = data[train] #testing subjs
-        test_mixed = reshape.iNets_OS('mixed', test_subs)
-        test_slow = reshape.iNets_OS('slowreveal', test_subs)
-        testing_set_rest = reshape.iNets_OS('rest', test_subs)
+        test_mixed = iNets_OS('mixed', test_subs)
+        test_slow = iNets_OS('slowreveal', test_subs)
+        testing_set_rest = iNets_OS('rest', test_subs)
         testing_set_task = np.concatenate((test_mixed, test_slow))
         y_test_task=np.ones(testing_set_task.shape[0])
         y_test_rest=np.zeros(testing_set_rest.shape[0])
@@ -153,14 +147,14 @@ def classifyALL_Binary_INET():
             train_sub_ses = sessions[train_ses]
             val_sub_ses = sessions[test_ses]
 
-            slowFC = reshape.iNets_SS('slowreveal', train_sub[0], train_sub_ses)
-            mixFC = reshape.iNets_SS('mixed', train_sub[0], train_sub_ses)
-            val_slow = reshape.iNetOpenSes('slowreveal', train_sub[0], val_sub_ses[0])
-            val_mix = reshape.iNetOpenSes('mixed', train_sub[0], val_sub_ses[0])
-            restFC = reshape.iNets_SS('rest', train_sub[0], train_sub_ses)
-            val_rest = reshape.iNetOpenSes('rest', train_sub[0], val_sub_ses[0])
+            slowFC = iNets_SS('slowreveal', train_sub[0], train_sub_ses)
+            mixFC = iNets_SS('mixed', train_sub[0], train_sub_ses)
+            val_slow = iNetOpenSes('slowreveal', train_sub[0], val_sub_ses[0])
+            val_mix = iNetOpenSes('mixed', train_sub[0], val_sub_ses[0])
+            restFC = iNets_SS('rest', train_sub[0], train_sub_ses)
+            val_rest = iNetOpenSes('rest', train_sub[0], val_sub_ses[0])
             taskFC = np.concatenate((mixFC, slowFC))
-
+            val_task=np.concatenate((val_mix, val_slow))
             t = np.ones(taskFC.shape[0], dtype = int)
             r=np.zeros(restFC.shape[0], dtype=int)
             y = np.concatenate((t,r))
@@ -184,7 +178,7 @@ def classifyALL_Binary_INET():
     dfDS['SS'] = SStotal_acc
     dfDS['OS'] = OStotal_acc
     df=pd.melt(dfDS,id_vars=['subs'],value_vars=['SS','OS'],var_name='Analysis',value_name='acc')
-    df.to_csv(outDir+'Ridge/ALL_Binary/ALL_iNets_acc.csv',index=False)
+    df.to_csv(outDir+'ALLBinary_iNets_acc.csv',index=False)
 
 
 
@@ -226,28 +220,33 @@ def classifyALL_MC_INET():
         OSacc_folds =[]
         train_sub = data[test] #training subj
         test_subs = data[train] #testing subjs
-        test_mixed = reshape.iNets_OS('mixed', test_subs)
-        test_slow = reshape.iNets_OS('slowreveal', test_subs)
-        testing_set_rest = reshape.iNets_OS('rest', test_subs)
+        test_mixed = iNets_OS('mixed', test_subs)
+        test_slow = iNets_OS('slowreveal', test_subs)
+        testing_set_rest = iNets_OS('rest', test_subs)
+        #make sure rest and task sets are the same amount
+        testing_set_rest=testing_set_rest[np.random.choice(testing_set_rest.shape[0], test_mixed.shape[0], replace=False), :]
         y_test_slow = np.full(test_slow.shape[0],2)
         y_test_mix=np.ones(test_mixed.shape[0])
         y_test_rest=np.zeros(testing_set_rest.shape[0])
         ytest=np.concatenate((y_test_slow,y_test_mix,y_test_rest))
         Xtest=np.concatenate((test_slow,test_mixed,testing_set_rest))
-        same_sub_CM=np.zeros((3,3))
-        diff_sub_CM=np.empty((4,3,3)) #number of sessions is the number of splits
+        #same_sub_CM=np.zeros((3,3))
+        #diff_sub_CM=np.empty((4,3,3)) #number of sessions is the number of splits
         count = 0
         diff_count=0
         for train_ses, test_ses in loo.split(sessions): #leave one session out
             train_sub_ses = sessions[train_ses]
             val_sub_ses = sessions[test_ses]
 
-            slowFC = reshape.iNets_SS('slowreveal', train_sub[0], train_sub_ses)
-            mixFC = reshape.iNets_SS('mixed', train_sub[0], train_sub_ses)
-            val_slow = reshape.iNetOpenSes('slowreveal', train_sub[0], val_sub_ses[0])
-            val_mix = reshape.iNetOpenSes('mixed', train_sub[0], val_sub_ses[0])
-            restFC = reshape.iNets_SS('rest', train_sub[0], train_sub_ses)
-            val_rest = reshape.iNetOpenSes('rest', train_sub[0], val_sub_ses[0])
+            slowFC = iNets_SS('slowreveal', train_sub[0], train_sub_ses)
+            mixFC = iNets_SS('mixed', train_sub[0], train_sub_ses)
+            val_slow = iNetOpenSes('slowreveal', train_sub[0], val_sub_ses[0])
+            val_mix = iNetOpenSes('mixed', train_sub[0], val_sub_ses[0])
+            restFC = iNets_SS('rest', train_sub[0], train_sub_ses)
+            val_rest = iNetOpenSes('rest', train_sub[0], val_sub_ses[0])
+            #make sure rest and task sets are the same amount
+            val_rest=val_rest[np.random.choice(val_rest.shape[0], val_mix.shape[0], replace=False), :]
+            restFC=restFC[np.random.choice(restFC.shape[0], mixFC.shape[0], replace=False), :]
 
             s = np.full(slowFC.shape[0], 2)
             t = np.ones(mixFC.shape[0], dtype = int)
@@ -266,28 +265,160 @@ def classifyALL_MC_INET():
             #Other subject
             OSscores=clf.score(Xtest,ytest)
             OSacc_folds.append(OSscores)
-            y_predict=clf.predict(X_val)
-            cm_same = confusion_matrix(y_val, y_predict)
-            same_sub_CM=same_sub_CM+cm_same
+            #y_predict=clf.predict(X_val)
+            #cm_same = confusion_matrix(y_val, y_predict)
+            #same_sub_CM=same_sub_CM+cm_same
 
             #order check confusion matrices
-            y_pre=clf.predict(Xtest)
-            cm_diff = confusion_matrix(ytest, y_pre)
-            diff_sub_CM[diff_count]=cm_diff
-            diff_count=diff_count+1
-            DS_cm=diff_sub_CM.mean(axis=0,dtype=int)
+            #y_pre=clf.predict(Xtest)
+            #cm_diff = confusion_matrix(ytest, y_pre)
+            #diff_sub_CM[diff_count]=cm_diff
+            #diff_count=diff_count+1
+            #DS_cm=diff_sub_CM.mean(axis=0,dtype=int)
 
         OStotal_acc.append(mean(OSacc_folds))
         SStotal_acc.append(mean(SSacc_folds))
-        DS=DS_cm / DS_cm.astype(np.float).sum(axis=1)
-        CV=same_sub_CM / same_sub_CM.astype(np.float).sum(axis=1)
-        all_CM_DS=DS_cm+all_CM_DS
-        all_CM_CV=same_sub_CM+all_CM_CV
-    finalDS=all_CM_DS / all_CM_DS.astype(np.float).sum(axis=1)
-    finalCV=all_CM_CV / all_CM_CV.astype(np.float).sum(axis=1)
+        #DS=DS_cm / DS_cm.astype(np.float).sum(axis=1)
+        #CV=same_sub_CM / same_sub_CM.astype(np.float).sum(axis=1)
+        #all_CM_DS=DS_cm+all_CM_DS
+        #all_CM_CV=same_sub_CM+all_CM_CV
+    #finalDS=all_CM_DS / all_CM_DS.astype(np.float).sum(axis=1)
+    #finalCV=all_CM_CV / all_CM_CV.astype(np.float).sum(axis=1)
     dfDS['subs']=subList
     dfDS['SS'] = SStotal_acc
     dfDS['OS'] = OStotal_acc
     df=pd.melt(dfDS,id_vars=['subs'],value_vars=['SS','OS'],var_name='Analysis',value_name='acc')
-    df.to_csv(outDir+'Ridge/ALL_MC/iNets_acc.csv',index=False)
-    return finalDS, finalCV #to make confusion matrices
+    df.to_csv(outDir+'ALL_MC_iNets_acc.csv',index=False)
+    #np.savetxt(outDir+'CM_DS.csv', finalDS)
+    #np.savetxt(outDir+'CM_SS.csv', finalCV)
+
+
+def iNets_SS(train_task, sub, sesList):
+    """
+    Calculate FC all sessions matrices in list for a sub in a given task
+    Parameters
+    -----------
+    df : str
+        Path to file
+    Returns
+    -----------
+    ds : 2D upper triangle FC measures in (roi, days) format
+
+    """
+    result_arr = []
+    #loop through and append all test sets do each task/rest separate
+    for session in sesList:
+        all_ses_mats = iNetOpenSes(train_task, sub, session)
+        result_arr.append(all_ses_mats)
+    result_arr = np.concatenate(result_arr)
+    return result_arr
+
+def iNets_OS(train_task, test_subs):
+    """
+    Calculate FC matrices for all subs in a given task
+    Parameters
+    -----------
+    df : str
+        Path to file
+    Returns
+    -----------
+    ds : 2D upper triangle FC measures in (roi, days) format
+
+    """
+    result_arr = []
+    #loop through and append all test sets do each task/rest separate
+    for sub in test_subs:
+        all_sub_mats = iNetOpenALL(train_task, sub)
+        result_arr.append(all_sub_mats)
+    result_arr = np.concatenate(result_arr)
+    return result_arr
+
+def iNetOpenALL(train_task, sub):
+    """
+    Convert matlab files into upper triangle np.arrays for a given sub (all sessions/runs)
+    Parameters
+    -----------
+    df : str
+        Path to file
+    Returns
+    -----------
+    ds : 2D upper triangle FC measures in (roi, days) format
+
+    """
+    #find all files
+    files = glob.glob(dataDir+train_task+'/'+sub+'_ses-*')
+    nsess = len(files) #FC matrices
+    nrois=333
+    ds=np.empty((nsess, int(nrois*(nrois-1)/2)))
+    count=0
+    for f in files:
+    #Consistent parameters to use for editing datasets
+
+        #Load FC file
+        fileFC=scipy.io.loadmat(f)
+
+        #Convert to numpy array
+        fileFC=np.array(fileFC['parcel_corrmat'])
+        #Replace nans and infs with zero
+        fileFC=np.nan_to_num(fileFC)
+        #Index upper triangle of matrix
+        mask=np.triu_indices(nrois,1)
+
+    #Loop through all 10 days to reshape correlations into linear form
+
+        tmp=fileFC[:,:]
+        ds[count]=tmp[mask]
+        count=count+1
+    mask = (ds == 0).all(1)
+    column_indices = np.where(mask)[0]
+    df = ds[~mask,:]
+    return df
+
+
+def iNetOpenSes(train_task, sub, ses):
+    """
+    Convert matlab files into upper triangle np.arrays for a given session
+    Parameters
+    -----------
+    df : str
+        Path to file
+    Returns
+    -----------
+    ds : 2D upper triangle FC measures in (roi, days) format
+
+    """
+    #find all files in a given session
+    files = glob.glob(dataDir+train_task+'/'+sub+'_'+ses+'*')
+    nsess = len(files) #FC matrices
+    nrois=333
+    ds=np.empty((nsess, int(nrois*(nrois-1)/2)))
+    count=0
+    for f in files:
+    #Consistent parameters to use for editing datasets
+
+        #Load FC file
+        fileFC=scipy.io.loadmat(f)
+
+        #Convert to numpy array
+        fileFC=np.array(fileFC['parcel_corrmat'])
+        #Replace nans and infs with zero
+        fileFC=np.nan_to_num(fileFC)
+        #Index upper triangle of matrix
+        mask=np.triu_indices(nrois,1)
+
+    #Loop through all 10 days to reshape correlations into linear form
+
+        tmp=fileFC[:,:]
+        ds[count]=tmp[mask]
+        count=count+1
+    mask = (ds == 0).all(1)
+    column_indices = np.where(mask)[0]
+    df = ds[~mask,:]
+    return df
+
+
+for task in taskList:
+    classifyDS_INET(task)
+#classifyDS_INET('mixed')
+classifyALL_Binary_INET()
+classifyALL_MC_INET()
